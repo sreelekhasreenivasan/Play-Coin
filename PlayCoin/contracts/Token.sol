b1afe8de7f16f9a19aea1c005d68db53beabed63
sreelekhasreenivasan123@gmail.com <Sree@20121999>
2025-01-10 09:19:57 +0530

    add

---
+++ PlayCoin/contracts/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC20, Ownable {
+    constructor()
+        ERC20("Play Coin", "PCOIN")
+        Ownable(0x180865399db80d676F21a9F5e55b2a882a9FD03c)
+    {
+        _mint(msg.sender, 10000);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
